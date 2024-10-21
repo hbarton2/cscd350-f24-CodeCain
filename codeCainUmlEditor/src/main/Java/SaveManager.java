73387f398c01b1fb69035d1693665a01245b0bf4
colbycrutcher <94940284+colbycrutcher@users.noreply.github.com>
2024-10-20 18:23:50 -0700

    start maven

---
+++ codeCainUmlEditor/src/main/Java/SaveManager.java
@@ -0,0 +1,16 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
+public class SaveManager {
+
+    public static void saveUMLToJson(Object umlObject, String filePath) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File(filePath), umlObject);
+            System.out.println("UML saved to JSON file successfully.");
+        } catch (IOException e) {
+            System.err.println("Error saving UML to JSON file: " + e.getMessage());
+        }
+    }
+}
