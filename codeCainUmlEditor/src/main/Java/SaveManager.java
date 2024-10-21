19d405dd3f38fb48ea0e2f3647d0f40bb593aa2d
rrudolfo <rrudolfo@ewu.edu>
2024-10-20 19:02:23 -0700

    Load

--- codeCainUmlEditor/src/main/Java/SaveManager.java
+++ codeCainUmlEditor/src/main/Java/SaveManager.java
@@ -2,6 +2,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 
+
 public class SaveManager {
 
     public static void saveUMLToJson(Object umlObject, String filePath) {
@@ -12,5 +13,17 @@ public class SaveManager {
         } catch (IOException e) {
             System.err.println("Error saving UML to JSON file: " + e.getMessage());
         }
+    }
+
+    public static <T> T loadUMLFromJson(String filePath, Object umlObject ){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(new File(filePath), umlObject);
+            System.out.println("Loaded your previously saved data");
+        } catch (Exception e) {
+            System.err.println("Error loading previously saved data" + e.getMessage()); 
+            return null;
+        }
+
     }
 }
