890543c2c2e0a03f487aeed3d7d1ca3b42858beb
aoehlerewu <166942126+aoehlerewu@users.noreply.github.com>
2024-10-25 19:36:23 -0700

    Created UMLParameterInfo Class
    Added UMLParameterInfo to store any parameter name and types to the method class.

---
+++ UML editor/src/main/java/codecain/UMLParameterInfo.java
@@ -0,0 +1,78 @@
+package codecain;
+
+
+
+/**
+ * The UMLParameterInfo class represents a parameter in a UML class.
+ * It includes the parameter name and the parameter type.
+ */
+public class UMLParameterInfo {
+
+    /**
+     * The name of the parameter.
+     */
+    private String parameterName;
+
+    /**
+     * The type of the parameter.
+     */
+    private String parameterType;
+
+    /**
+     * Constructs a UMLParameterInfo object with the specified field name and parameter type.
+     *
+     * @param parameterName the name of the parameter
+     * @param parameterType the type of the parameter
+     */
+    public UMLParameterInfo(String parameterName, String parameterType) {
+        this.parameterName = parameterType;
+        this.parameterType = parameterName;
+    }
+
+    /**
+     * Gets the name of the parameter.
+     *
+     * @return the name of the parameter
+     */
+    public String getParameterName() {
+        return parameterName;
+    }
+
+    /**
+     * Gets the type of the parameter.
+     *
+     * @return the type of the parameter
+     */
+    public String getParameterType() {
+        return parameterType;
+    }
+
+    /**
+     * Sets the name of the parameter.
+     *
+     * @param parameterName the new name of the parameter
+     */
+    public void setParameterName(String parameterName) {
+        this.parameterName = parameterName;
+    }
+
+    /**
+     * Sets the type of the parameter.
+     *
+     * @param parameterType the new type of the parameter
+     */
+    public void setParameterType(String parameterType) {
+        this.parameterType = parameterType;
+    }
+
+    /**
+     * Returns a string representation of the UMLParameterInfo object, showing the parameter name and type.
+     *
+     * @return a string in the format "parameterType : parameterName"
+     */
+    @Override
+    public String toString() {
+        return parameterType + " : " + parameterName;
+    }
+}
+
