1004701cc2da733bf89d08db7a6366b1bd7844f4
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-11-06 13:10:35 -0800

    feature: class node and drag

--- UML editor/src/main/java/codecain/Controller.java
+++ UML editor/src/main/java/codecain/Controller.java
@@ -2,14 +2,27 @@ package codecain;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller {
 
     @FXML
-    private Label helloLabel;
+    private AnchorPane classContainer;
 
     @FXML
-    private void onClickMeButton() {
-        helloLabel.setText("Welcome!!!");
+    private void addClassBtn() {
+        ClassNode classNode = new ClassNode("New Class");
+
+        // Calculate the center of the canvas
+        double centerX = (classContainer.getWidth() - classNode.getPrefWidth()) / 2;
+        double centerY = (classContainer.getHeight() - classNode.getPrefHeight()) / 2;
+
+        // Set the position of the classNode to the center of the canvas
+        classNode.setLayoutX(centerX);
+        classNode.setLayoutY(centerY);
+
+        classContainer.getChildren().add(classNode);
+
+
     }
 }
