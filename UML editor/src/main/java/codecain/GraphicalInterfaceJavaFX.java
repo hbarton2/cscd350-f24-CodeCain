6080ddf22111e89fe87caeaac7c67acab0f48266
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-11-06 19:52:23 -0800

    feature: menu btn styles

--- UML editor/src/main/java/codecain/GraphicalInterfaceJavaFX.java
+++ UML editor/src/main/java/codecain/GraphicalInterfaceJavaFX.java
@@ -6,15 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class GraphicalInterfaceJavaFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GraphicalInterface.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("GraphicalInterface.fxml"));
+            String css = Objects.requireNonNull(this.getClass().getResource("application.css")).toExternalForm();
             Parent root = loader.load();
             Scene scene = new Scene(root);
-            stage.setTitle("Hello!");
+            scene.getStylesheets().add(css);
+
+            stage.setTitle("UML Editor");
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
