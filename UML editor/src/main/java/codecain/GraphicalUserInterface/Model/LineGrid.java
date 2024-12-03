5116c819203e88965e5340bf684bdae9490aae1e
benFoster1234567 <bfoster13@ewu.edu>
2024-12-03 13:41:13 -0800

    Update LineGrid.java
    Updated the line grid to correctly determine the locations of the VBoxes

--- UML editor/src/main/java/codecain/GraphicalUserInterface/Model/LineGrid.java
+++ UML editor/src/main/java/codecain/GraphicalUserInterface/Model/LineGrid.java
@@ -3,6 +3,8 @@ package codecain.GraphicalUserInterface.Model;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 /**
  * grids must interact with classes and arrows
@@ -26,28 +28,63 @@ public class LineGrid {
     private final double cellWidth;
     private GridCell[][] grid;
     private boolean isGenerated;
+
+
     private int numRows;
     private int numCols;
     private final Pane nodeContainer;
+    private double screenWidth;
+    private double screenHeight;
+
+
 
     /**
      * Constructor for GridManager
      * @param cellWidth the width of each cell
      * @param nodeContainer the pane containing the classNodes
      */
-    public LineGrid(double cellWidth, Pane nodeContainer){
+    public LineGrid(double cellWidth, double screenWidth, double screenHeight, Pane nodeContainer) {
         this.nodeContainer = nodeContainer;
         isGenerated = false;
         this.cellWidth = cellWidth;
-        initGrid();
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        try {
+            initGrid();
+        } catch (Exception error) {
+            System.out.println("Error during grid initialization: " + error.getMessage());
+            throw new RuntimeException("Grid initialization failed", error);
+        }
+        drawTestDot(100.0,100.0, nodeContainer);
+    }
+
+    public double getCellWidth(){
+        return this.cellWidth;
+    }
+
+    /**
+     * draws a dot to mark a certain point for testing
+     * @param x x value of the dot
+     * @param y y value of the dot
+     * @param pane the pane to draw the dot on
+     */
+    private void drawTestDot(double x, double y, Pane pane){
+        Circle dot = new Circle();
+        dot.setCenterX(x);
+        dot.setCenterY(y);
+        dot.setRadius(3.0);
+        dot.setFill(Color.BLACK);
+        pane.getChildren().add(dot);
+        dot.toFront();
     }
 
     /**
      * generates a grid. Should only be called in the constructor
      */
-    private void initGrid(){
-        numRows = (int) (nodeContainer.getWidth() / cellWidth);
-        numCols = (int) (nodeContainer.getHeight() / cellWidth);
+    private void initGrid() throws Exception {
+        this.numRows = (int) (screenWidth / cellWidth);
+        this.numCols = (int) (screenHeight / cellWidth);
+        checkConstructorErrors();
         if (isGenerated){
             return;
         }
@@ -55,8 +92,32 @@ public class LineGrid {
         generateGrid();
     }
 
+    /**
+     * validates the dimensions of the screen to make sure everything is created properly
+     */
+    private void checkConstructorErrors() throws Exception {
+        if (screenHeight == 0 || screenWidth == 0){
+            String errorMsg = "";
+            if (screenHeight == 0){
+                errorMsg += "screenHeight = 0! ";
+            }
+            if (screenWidth == 0) {
+                errorMsg += "screenWidth = 0!";
+            }
+            throw new Exception(errorMsg + " Screen width and Height must be set!");
+        }
+
+        if (numRows == 0 || numCols == 0){
+            throw new Exception("Grid cannot have a width or height of 0");
+        }
+    }
+
+    /**
+     * helper method to generate the grid
+     */
     private void generateGrid(){
         grid = new GridCell[numRows][numCols];
+        System.out.println("New grid created: numCols: " + numCols + ", numRows: " + numRows);
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
                 grid[row][col] = new GridCell(false, 1.0); // Default state
@@ -97,16 +158,14 @@ public class LineGrid {
      * @param classNode the VBox representing the current classNode
      */
     private void updateClassBox(VBox classNode){
-        double screenWidth = nodeContainer.getWidth();
-        double screenHeight = nodeContainer.getHeight();
         double nodeWidth = classNode.getWidth();
         double nodeHeight = classNode.getHeight();
         double nodeX = classNode.getLayoutX();
         double nodeY = classNode.getLayoutY();
         int rowStart = getRow(nodeY);
         int rowEnd = getRow(nodeY + nodeHeight);
-        int colStart = getCol(nodeY);
-        int colEnd = getCol(nodeY + nodeHeight);
+        int colStart = getCol(nodeX);
+        int colEnd = getCol(nodeX + nodeWidth);
         for (int row = rowStart; row < rowEnd; row++){
             for (int col = colStart; col < colEnd; col++){
                 if (row >= 0 && row < numRows && col >= 0 && col < numCols) {
@@ -114,6 +173,7 @@ public class LineGrid {
                 }
             }
         }
+        System.out.printf("\nOccupied at 100,100: %c\n", checkOccupied(100.0,100.0)? 'T' : 'F');
     }
 
     /**
@@ -155,8 +215,8 @@ public class LineGrid {
      * @param x the coordinate x value
      * @return the index for the x value
      */
-    private int getRow(double x) {
-        int row = (int)(x / cellWidth);
+    private int getRow(double y) {
+        int row = (int)(y / cellWidth);
         return Math.max(0, Math.min(row, numRows - 1));
     }
 
@@ -165,8 +225,29 @@ public class LineGrid {
      * @param y the coordinate y value
      * @return the index of the column of the coordinate y value
      */
-    private int getCol(double y) {
-        int col = (int)(y / cellWidth);
+    private int getCol(double x) {
+        int col = (int)(x / cellWidth);
         return Math.max(0, Math.min(col, numCols - 1));
+    }
+
+
+    public double getScreenHeight() {
+        return screenHeight;
+    }
+
+    public double getScreenWidth() {
+        return screenWidth;
+    }
+
+    public int getNumCols() {
+        return numCols;
+    }
+
+    public int getNumRows() {
+        return numRows;
+    }
+
+    public Pane getNodeContainer() {
+        return nodeContainer;
     }
 }
