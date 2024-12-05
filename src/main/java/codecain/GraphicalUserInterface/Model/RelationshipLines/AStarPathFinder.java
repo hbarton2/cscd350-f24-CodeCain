64d43cddb0170993206d1d0b60595ed86deb5915
benFoster1234567 <bfoster13@ewu.edu>
2024-12-04 19:29:29 -0800

    Create AStarPathFinder.java
    Created an AStarPathFinder class

---
+++ src/main/java/codecain/GraphicalUserInterface/Model/RelationshipLines/AStarPathFinder.java
@@ -0,0 +1,24 @@
+package codecain.GraphicalUserInterface.Model.RelationshipLines;
+
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class AStarPathFinder {
+
+    private final LineGrid grid;
+
+    private Queue<GridCell> priorityQueue;
+
+    public AStarPathFinder(LineGrid grid) {
+        this.grid = grid;
+
+    }
+
+    public ArrayList<GridCell> findPath(GridCell start, GridCell end){
+        return null;
+    }
+
+
+
+
+}
