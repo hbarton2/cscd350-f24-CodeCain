b33837f531e0e060968479c5edc9fc7e6746673f
benFoster1234567 <bfoster13@ewu.edu>
2024-12-06 23:25:34 -0800

    Create RelationshipPathHolder.java
    Created a class to hold relationships and map them to their paths

---
+++ src/main/java/codecain/GraphicalUserInterface/Model/RelationshipLines/RelationshipPathHolder.java
@@ -0,0 +1,117 @@
+package codecain.GraphicalUserInterface.Model.RelationshipLines;
+
+
+import codecain.BackendCode.Model.Relationship;
+import codecain.GraphicalUserInterface.Controller.Controller;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * this is a class to take relationships and turn them into paths.
+ * Also maps relatioinships to the classNodes from the GUI
+ *
+ */
+public class RelationshipPathHolder {
+
+    /**
+     * helper class to hold the source and destination nodes of the relationship line
+     */
+    private static class RelBoxHolder{
+        VBox source;
+        VBox destination;
+
+        RelBoxHolder (VBox source, VBox destination){
+            this.source = source;
+            this.destination = destination;
+        }
+
+        VBox getSource(){
+            return this.source;
+        }
+
+        VBox getDestination(){
+            return this.destination;
+        }
+    }
+
+
+    /**
+     * finds the classboxes from the relationship?
+     */
+    private HashMap<Relationship, RelBoxHolder> classBoxHolder;
+    private HashMap<Relationship, GridPath> paths;
+    private PathNavigator navigator;
+    private Controller controller;
+
+    public RelationshipPathHolder(PathNavigator navigator, Controller controller){
+        this.classBoxHolder = new HashMap<>();
+        this.paths = new HashMap<>();
+        this.navigator=navigator;
+        this.controller = controller;
+    }
+
+    public VBox getSourceClassNode(Relationship r){
+        return this.classBoxHolder.get(r).getSource();
+    }
+
+    public VBox getDestinationClassNode(Relationship r){
+        return this.classBoxHolder.get(r).getDestination();
+    }
+
+    public ArrayList<GridPath> loadPathsFromRelationships(ArrayList<Relationship> relationships){
+        ArrayList<GridPath> newPaths = new ArrayList<>();
+        for(Relationship r : relationships){
+            newPaths.add(this.paths.get(r));
+        }
+        return newPaths;
+    }
+
+    /**
+     * must be called every time a relationship is added
+     * @param relationship the relationship to add
+     * @param path the path of the relationship
+     */
+    public void addRelationshipPath(Relationship relationship, GridPath path){
+        paths.put(relationship, path);
+
+    }
+
+    public void addRelationshipHolder(Relationship relationship){
+        VBox sourceBox = controller.findClassNode(relationship.getSource());
+        VBox destBox = controller.findClassNode(relationship.getDestination());
+        RelBoxHolder h = new RelBoxHolder(sourceBox, destBox);
+        classBoxHolder.put(relationship, h);
+    }
+
+    /**
+     * must be called every time a relationship is deleted
+     * @param relationship the relationship to remove
+     * @return the removed class
+     */
+    public GridPath removeRelationshipPath(Relationship relationship){
+        GridPath p = paths.get(relationship);
+        paths.remove(relationship);
+        classBoxHolder.remove(relationship);
+        return p;
+    }
+
+    public ArrayList<GridPath> getPathsToArrayList(){
+        return new ArrayList<>(this.paths.values());
+    }
+
+    public void clearHolder(){
+        this.classBoxHolder.clear();
+        this.paths.clear();
+    }
+
+    public GridPath getPath(Relationship relationship){
+        return paths.get(relationship);
+    }
+
+
+
+}
