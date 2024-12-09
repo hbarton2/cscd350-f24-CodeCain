8e41845fa14c146e06a34f18802d6cc3314c1f94
colbycrutcher <94940284+colbycrutcher@users.noreply.github.com>
2024-12-08 23:57:55 -0800

    HOTFIX - fix window size so it can load

--- src/main/java/codecain/GraphicalUserInterface/Controller/Controller.java
+++ src/main/java/codecain/GraphicalUserInterface/Controller/Controller.java
@@ -104,7 +104,7 @@ public class Controller {
         });
 
         //loadGridManager needs to be called when the load button is pressed. The arguments should be the same as below
-        GridManager.getInstance().setGrid(new LineGrid(50.0,2000.0,2000.0, nodeContainer),this);
+        GridManager.getInstance().setGrid(new LineGrid(50.0,6000.0,6000.0, nodeContainer),this);
         GridManager.setVisualizer();
         RelationshipManager.setController(this);
 
@@ -295,7 +295,7 @@ public class Controller {
             populateGUIFromClassMap();
         }
 
-        GridManager.loadGridManager(new LineGrid(50.0,2000.0,2000.0, nodeContainer),
+        GridManager.loadGridManager(new LineGrid(50.0,6000.0,6000.0, nodeContainer),
                 this, nodeContainer);
 
 
